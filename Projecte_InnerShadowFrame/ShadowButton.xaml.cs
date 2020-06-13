952307f261f5141f431ebf3af376b9b53a432504
Enric Pedrós <enric.pedros@col.vueling.com>
2020-06-13 02:51:57 +0200

    [InnerShadow] ShadowButton

---
+++ Projecte_InnerShadowFrame/ShadowButton.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projecte_InnerShadowFrame
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShadowButton : ContentView
+    {
+        public ShadowButton()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Pressed(object sender, EventArgs e)
+        {
+            await Task.Run(async () => await Task.Delay(100));
+            ButtonElement.BackgroundColor = Color.FromHex("#333EBD");
+        }
+
+        private async void ButtonElement_Released(object sender, EventArgs e)
+        {
+            await Task.Run(async () => await Task.Delay(100));
+            ButtonElement.BackgroundColor = Color.Transparent;
+        }
+    }
+}
